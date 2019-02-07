1e969a10300a534046b815bbf2da93f455549884
Conor <cwilson0005@hotmail.co.uk>
2019-02-07 12:31:09 +0000

    Started journey to find the munro data, we found it

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
+const Munros = require('./models/munros.js');
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
-})
+
+  const munrosList = document.querySelector('.list-of-munros');
+
+  const munroTest = new Munros();
+  munroTest.getData();
+});
---
+++ src/models/munros.js
@@ -0,0 +1,28 @@
+const RequestHelper = require('../helpers/request_helper.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+const Munros = function () {
+  this.data = null;
+};
+
+Munros.prototype.bindEvents = function () {
+
+};
+
+Munros.prototype.getData = function () {
+  const url = "https://munroapi.herokuapp.com/munros";
+
+  const request = new RequestHelper(url);
+  console.log(request);
+  request.get()
+    .then((munros) => {
+      console.log(munros);
+      this.data = munros;
+      PubSub.publish('Munros:data-ready', this.data);
+    })
+    .catch((err) => {
+      PubSub.publish('Munros:error', err);
+    });
+};
+
+module.exports = Munros;
